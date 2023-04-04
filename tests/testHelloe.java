b65c5a4c9ef0c3e6ba04042a3f80adcebfb36c5b
Asanka Perera <asanka@calcey.com>
2023-04-04 22:59:52 +0530

    new: add test case.

---
+++ tests/testHelloe.java
@@ -0,0 +1,12 @@
+/**
+ * This the test class for Hello.java
+ */
+public class testHello {
+    /**
+     * This is the test method for Hello.java
+     */
+    @Test
+    public void testHello() {
+        assertEquals(Hello.add(1, 2), 3);
+    }
+}
